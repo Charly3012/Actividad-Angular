1bf0d87bc592a5dd831381fef1b469a1531087d7
Charly3012 <carlocoronadosilva975@gmail.com>
2024-10-15 11:58:32 -0600

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculadoraComponent } from './components/calculadora/calculadora.component';
+import { DivisasComponent } from './components/divisas/divisas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : "calculadora",
+    component : CalculadoraComponent
+  },
+  {
+    path : "divisas",
+    component : DivisasComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculadoraComponent } from './components/calculadora/calculadora.component';
+import { DivisasComponent } from './components/divisas/divisas.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculadoraComponent,
+    DivisasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
